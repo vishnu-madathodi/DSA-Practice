e51fcebd9d5f409f123a200fbb49b44882807a79
MR POOL <71345322+vishnu-madathodi@users.noreply.github.com>
2026-02-25 23:41:17 +0530

    Using two loops

---
+++ Longest Common Prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty() || strs[0].empty()){
+            return "";
+        }
+        string prefix = "";
+        for(int i = 0; i < strs[0].length(); i++){
+            char currentChar = strs[0][i];
+            for(int j = 1; j < strs.size(); j++){
+                if(i >= strs[j].length() || strs[j][i] != currentChar){
+                    return prefix;
+                }
+            }
+            prefix += currentChar;
+        }
+        return prefix;
+    }
+};
